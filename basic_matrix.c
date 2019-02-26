8692303d80332404b82acc60787d9dd6b6dcc232
Ravi <ravi571@outlook.com>
2019-02-26 15:01:17 +0530

    Sub-routines tested on basic inputs. Worked well

--- basic_matrix.c
+++ basic_matrix.c
@@ -1,6 +1,6 @@
 /* This file contains basic matrix operations which can be used multiple scenarios. Includes common opeartions like pseudo-inverse, transpose*/
 // Uses GNU Scientific Library: https://www.gnu.org/software/gsl/doc/html/linalg.html
-
+#include<time.h>
 #include<stdlib.h>
 #include<gsl/gsl_matrix.h>
 #include<gsl/gsl_vector.h>
@@ -52,24 +52,24 @@ void orthonormalize(gsl_matrix * matrix){
   gsl_vector * tau = gsl_vector_calloc(GSL_MIN (M, N));
   flag = gsl_linalg_QR_decomp(matrix, tau);
   if (flag==1)
-    printf("Error2!!");
+    printf("Error!!: Unable to orthonormalize; fn: orthonormalize\n");
 
   gsl_matrix * Q = gsl_matrix_alloc(M, M);
   gsl_matrix * R = gsl_matrix_alloc(M, N);
   flag = gsl_linalg_QR_unpack(matrix, tau, Q, R);
   gsl_vector_free(tau);
   if (flag==1)
-    printf("Error1!!");
+    printf("Error!!: Unable to unpack; fn: orthonormalize\n");
   gsl_matrix_free(R);
   gsl_vector * temp = gsl_vector_alloc(M);
 
   for(int i=0; i<N; i++){
     flag = gsl_matrix_get_col(temp, Q, i);
     if (flag==1)
-      printf("Error!!");
+      printf("Error!!: Unable to copy column from matrix; fn: orthonormalize\n");
     flag = gsl_matrix_set_col(matrix, i, temp);
     if (flag==1)
-      printf("Error!!");
+      printf("Error!!: Unable to copy column to matrix; fn: orthonormalize\n");
   }
 
   gsl_vector_free(temp);
@@ -80,26 +80,28 @@ int matrix_mul(gsl_matrix * A, gsl_matrix * B, gsl_matrix * C){
   // Multiplies A and B in the usual fashion and sets C = AB
   // returns 0 or success and 1 otherwise
   if(A->size2 != B->size1){
-    printf("Error: Dimensions of A and B do not match\n");
+    printf("Error: Dimensions of A and B do not match; fn: matrix_mul\n");
     return 1;
   }
   else if(A->size1 != C->size1){
-    printf("Error: Dimensions of A and C do not match\n");
+    printf("Error: Dimensions of A and C do not match; fn: matrix_mul\n");
     return 1;
   }
   else if(B->size2 != C->size2){
-    printf("Error: Dimensions of B and C do not match\n");
+    printf("Error: Dimensions of B and C do not match; fn: matrix_mul\n");
     return 1;
   }
-  int temp;
+  double temp;
   for(int i=0; i< (C->size1); i++){
     for(int j=0; j< (C->size2); j++){
       temp = 0;
       for(int k=0; k< (B->size1); k++){
-        temp += (gsl_matrix_get(A, i, k)*gsl_matrix_get(B, k, j));
+        temp = temp + (gsl_matrix_get(A, i, k)*gsl_matrix_get(B, k, j));
       }
+      //printf("%lf ", temp);
       gsl_matrix_set(C, i, j, temp);
     }
+    //printf("\n");
   }
   return 0;
 }
@@ -111,10 +113,10 @@ int matrix_diagonal_product(gsl_vector * V, gsl_matrix * matrix, int type){
   type = 1 : mat(V) x matrix
   type = 2 : matrix x mat(V)
   */
-  int temp;
+  double temp;
   if(type==1){
     if(V->size != matrix->size1){
-      printf("Error: Dimensions do not match");
+      printf("Error: Dimensions do not match, fn: matrix_diagonal_product\n");
       return 1;
     }
     for(int i=0; i< (matrix->size1); i++){
@@ -127,11 +129,11 @@ int matrix_diagonal_product(gsl_vector * V, gsl_matrix * matrix, int type){
   }
   else if(type==2){
     if(V->size != matrix->size2){
-      printf("Error: Dimensions do not match");
+      printf("Error: Dimensions do not match, fn: matrix_diagonal_product\n");
       return 1;
     }
-    for(int i=0; i< (matrix->size1); i++){
-      for(int j=0; j< (matrix->size2); j++){
+    for(int j=0; j< (matrix->size2); j++){
+      for(int i=0; i< (matrix->size1); i++){
         temp = gsl_matrix_get(matrix, i, j);
         temp = temp*gsl_vector_get(V, j);
         gsl_matrix_set(matrix, i, j, temp);
@@ -139,26 +141,28 @@ int matrix_diagonal_product(gsl_vector * V, gsl_matrix * matrix, int type){
     }
   }
   else{
-    printf("Error: Incorrect type of multiplication specified");
+    printf("Error: Incorrect type of multiplication specified, fn: matrix_diagonal_product\n");
     return 1;
   }
   return 0;
 }
 
 gsl_matrix * pseudo_inverse(gsl_matrix * matrix){
-  int flag, M = matrix->size1, N = matrix->size2, temp;
+  int flag, M = matrix->size1, N = matrix->size2;
+  double temp;
   gsl_matrix * A = gsl_matrix_alloc(M, N);
 
   flag = gsl_matrix_memcpy(A, matrix);
   if (flag==1)
-    printf("Error!!");
+    printf("Error!!: Unable to copy matrix entries, fn:pseudo-inverse\n");
+
   gsl_vector * work = gsl_vector_alloc(N);
   gsl_vector * S = gsl_vector_alloc(N);
   gsl_matrix * V = gsl_matrix_alloc(N, N);
   // A = U x Sigma x V_T . The below function overwrites A with U. S = diag(Sigma). V is the transpose of V Transpose
   flag = gsl_linalg_SV_decomp(A, V, S, work);
   if (flag==1)
-    printf("Error!!");
+    printf("Error!!: Unable to get SVD decomposition, fn:pseudo-inverse\n");
   // Inverse of Sigma Matrix
   for(int i=0; i<N; i++){
     temp = gsl_vector_get(S, i);
@@ -167,13 +171,13 @@ gsl_matrix * pseudo_inverse(gsl_matrix * matrix){
   }
   flag = matrix_diagonal_product(S, V, 2);
   if (flag==1)
-    printf("Error!!");
-  C = gsl_matrix_alloc(M, N);
-  gsl_matrix * A_T = gsl_matrix_alloc(A);
+    printf("Error!!: Unable to get matrix diagonal product, fn:pseudo-inverse\n");
+  gsl_matrix * C = gsl_matrix_alloc(N, M);
+  gsl_matrix * A_T = transpose(A);
   gsl_matrix_free(A);
   flag = matrix_mul(V, A_T, C);
   if (flag==1)
-    printf("Error!!");
+    printf("Error!!: Unable to multiply matrices, fn:pseudo-inverse\n");
   gsl_vector_free(S);
   gsl_vector_free(work);
   gsl_matrix_free(A_T);
@@ -190,5 +194,56 @@ gsl_matrix * Generate_Matrix(int M, int N){
 }
 
 int main(){
-  
+  int flag;
+  for(int i=0; i<1; i++){
+    gsl_matrix * A = Generate_Matrix(5, 5);
+    printf("Matrix A\n");
+    print_matrix(A);
+    printf("\n");
+
+    gsl_matrix * A_T = transpose(A);
+    printf("Matrix A Transpose\n");
+    print_matrix(A_T);
+    printf("\n");
+    gsl_matrix * product_ortho1 = gsl_matrix_alloc(5, 5);
+    gsl_matrix * product_ortho2 = gsl_matrix_alloc(5, 5);
+    flag = matrix_mul(A, A_T, product_ortho1);
+    if (flag==1)
+      printf("Error!!");
+    flag = matrix_mul(A_T, A, product_ortho2);
+    if (flag==1)
+      printf("Error!!");
+    flag++;
+    printf("Orthonormal\n");
+    print_matrix(product_ortho1);
+    printf("\n");
+    print_matrix(product_ortho2);
+    printf("\n");
+    gsl_matrix_free(product_ortho1);
+    gsl_matrix_free(product_ortho2);
+
+    product_ortho1 = gsl_matrix_alloc(5, 5);
+    product_ortho2 = gsl_matrix_alloc(5, 5);
+    gsl_matrix_free(A_T);
+    gsl_matrix * inv = pseudo_inverse(A);
+    printf("Inverse of Matrix A\n");
+    print_matrix(inv);
+    printf("\n");
+    flag = matrix_mul(A, inv, product_ortho1);
+    if (flag==1)
+      printf("Error!!");
+    flag = matrix_mul(inv, A, product_ortho2);
+    if (flag==1)
+      printf("Error!!");
+    flag++;
+    printf("Inverse\n");
+    print_matrix(product_ortho1);
+    printf("\n");
+    print_matrix(product_ortho2);
+    printf("\n");
+    gsl_matrix_free(A);
+    gsl_matrix_free(product_ortho1);
+    gsl_matrix_free(product_ortho2);
+    gsl_matrix_free(inv);
+  }
 }
